7a067ff02e45b7a782693de7cef85043ecbf53e9
Krotav88 <krotav88@gmail.com>
2024-06-12 22:12:45 +0300

    init first screen

---
+++ lib/components/button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String text;
+  final void Function()? onTap;
+  const MyButton({super.key, required this.text, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 228, 97, 92),
+            borderRadius: BorderRadius.circular(10)),
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(width: 10),
+            const Icon(Icons.chevron_right, color: Colors.white),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_pizza/components/button.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 230, 34, 27),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const SizedBox(height: 25),
+              Text('RIGALOVKA 3D',
+                  style: GoogleFonts.dmSerifDisplay(
+                      fontSize: 28, color: Colors.white)),
+              const SizedBox(height: 25),
+              Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: Image.asset('lib/images/sushi-1.png'),
+              ),
+              Text('TUT VSE EBAT\' KAK VKUSNO',
+                  style: GoogleFonts.dmSerifDisplay(
+                      fontSize: 44, color: Colors.white)),
+              Text(
+                'Братан, жизнь — это движение. Держи удар, помогай своим и не сдавайся никогда',
+                style: TextStyle(color: Colors.grey[300], height: 2),
+              ),
+              const SizedBox(height: 25),
+              MyButton(
+                text: "Rigaem vmeste",
+                onTap: () {
+                  // go to menu
+                  Navigator.pushNamed(context, '/menupage');
+                },
+              )
+            ]),
+      ),
+    );
+  }
+}
